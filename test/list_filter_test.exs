d0a240a7b77b0f1b1e6ae56b3339456962fa86ec
marcsfel <marcsfelsouza@gmail.com>
2021-03-09 14:21:49 -0300

    create the test for call function

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,15 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "convert and return just thr odd numbers" do
+      list = ["1", "2", "7", "pedra"]
+
+      response = ListFilter.call(list)
+
+      expected_response = [1, 7]
+
+      assert response == expected_response
+    end
   end
 end
